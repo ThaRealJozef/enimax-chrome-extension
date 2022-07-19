a513ef2f75b2a9f9649ed40dfbf4aa334b3da4ac
enimax-anime <soitchu@proton.me>
2022-07-18 22:22:39 -0700

    Changed the value of config.chrome

--- www/extensions/index.js
+++ www/extensions/index.js
@@ -2,7 +2,7 @@ var config = {
     "local": localStorage.getItem("local") === "true",
     "remote": localStorage.getItem("remote"),
     "remoteWOport": localStorage.getItem("remoteWOport"),
-    "chrome" : false,
+    "chrome" : true,
 
 };
 
